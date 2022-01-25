2632e779dc991e00677f4b23f584769aa1f7be39
Andrew <fincheran@gmail.com>
2022-01-25 22:59:18 +0200

    add small fixes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from firebase_service import FirebaseService
 from gdrive_service import GdriveService
 from transport_files_service import TransportFilesService
 
-files_path = Path.cwd() / './files'
-firebase_secrets_path = Path.cwd() / './firebase_cred.json'
-firebase_app_config_path = Path.cwd() / './firebase_app_config.json'
+files_path = './files'
+firebase_secrets_path = './firebase_cred.json'
+firebase_app_config_path = './firebase_app_config.json'
 service = TransportFilesService(FirebaseService(firebase_secrets_path,firebase_app_config_path),GdriveService())
 service.start()
