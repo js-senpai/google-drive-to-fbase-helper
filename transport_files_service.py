8ba6c0041386d24af874c465b01350880e8661a9
Andrew <fincheran@gmail.com>
2022-01-25 22:39:48 +0200

    add small fixes

--- transport_files_service.py
+++ transport_files_service.py
@@ -12,49 +12,48 @@ class TransportFilesService:
         file_list = self.gdrive_service.ListFile(
             {'q': "title='Resource' and mimeType='application/vnd.google-apps.folder'"}).GetList()
         if len(file_list):
-            parent_id = ''
             for parent in file_list:
                 parent_id = parent['id']
                 print(f"Parent folder name: {parent['title']}")
-            if parent_id:
-                get_subfolders = self.gdrive_service.ListFile({'q': f"'{parent_id}' in parents and mimeType='application/vnd.google-apps.folder'"}).GetList()
-                if len(get_subfolders):
-                    filtered_files = []
-                    for child in get_subfolders:
-                        get_files = self.gdrive_service.ListFile(
-                            {'q': f"'{child['id']}' in parents"}).GetList()
-                        print(f"Subfolder name: {child['title']}")
-                        if len(get_files):
-                            for file in get_files:
-                                get_folder_name = re.findall('([0-9]+)_', file['title'], flags=re.IGNORECASE)
-                                print(f"File name: {file['title']}")
-                                if len(get_folder_name):
-                                    filtered_files.append({
-                                        'parent_id': child['id'],
-                                        'folder_name': get_folder_name[0],
-                                        'id': file['id'],
-                                        'name': file['title'],
-                                        'mimeType': file['mimeType']
-                                    })
-                                else:
-                                    print('Dont find title for file')
+                if parent_id:
+                    get_subfolders = self.gdrive_service.ListFile({'q': f"'{parent_id}' in parents and mimeType='application/vnd.google-apps.folder'"}).GetList()
+                    if len(get_subfolders):
+                        filtered_files = []
+                        for child in get_subfolders:
+                            get_files = self.gdrive_service.ListFile(
+                                {'q': f"'{child['id']}' in parents"}).GetList()
+                            print(f"Subfolder name: {child['title']}")
+                            if len(get_files):
+                                for file in get_files:
+                                    get_folder_name = re.findall('([0-9]+)_', file['title'], flags=re.IGNORECASE)
+                                    print(f"File name: {file['title']}")
+                                    if len(get_folder_name):
+                                        filtered_files.append({
+                                            'parent_id': child['id'],
+                                            'folder_name': get_folder_name[0],
+                                            'id': file['id'],
+                                            'name': file['title'],
+                                            'mimeType': file['mimeType']
+                                        })
+                                    else:
+                                        print('Dont find title for file')
+                            else:
+                                print(f"Files not found in subfolder {child['title']}")
+                        if len(filtered_files):
+                            for filtered_file in filtered_files:
+                                get_file = self.gdrive_service.CreateFile({'id': filtered_file['id']})
+                                get_file.GetContentFile(f"{self.files_path}/{filtered_file['name']}")
+                                path_to_file = f"{self.files_path}/{filtered_file['name']}"
+                                self.firebase_service.upload_files(filtered_file['folder_name'], filtered_file['name'],
+                                                                   path_to_file, filtered_file['mimeType'])
+                                # Remove file
+                                os.remove(path_to_file)
+                            print('Files successfully moved')
                         else:
-                            print(f"Files not found in subfolder {child['title']}")
-                    if len(filtered_files):
-                        for filtered_file in filtered_files:
-                            get_file = self.gdrive_service.CreateFile({'id': filtered_file['id']})
-                            get_file.GetContentFile(f"{self.files_path}/{filtered_file['name']}")
-                            path_to_file = f"{self.files_path}/{filtered_file['name']}"
-                            self.firebase_service.upload_files(filtered_file['folder_name'], filtered_file['name'],
-                                                               path_to_file, filtered_file['mimeType'])
-                            # Remove file
-                            os.remove(path_to_file)
-                        print('Files successfully moved')
+                            print('Filtered files array is empty')
                     else:
-                        print('Filtered files array is empty')
+                        print('Json folder not found in Resource folder')
                 else:
-                    print('Json folder not found in Resource folder')
-            else:
-                print('Subfolders not found in Resource folder')
+                    print('Subfolders not found in Resource folder')
         else:
             print('Resource folder not found.')
